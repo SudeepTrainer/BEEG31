a06e67533af8a5eaff2d91de99924632538a1c12
SudeepTrainer <info@icenbake.com>
2023-11-23 16:44:22 +0530

    23-11-2023

--- middleware/server.js
+++ middleware/server.js
@@ -9,7 +9,11 @@ function loggingMiddleware(req,res,next){
     console.log("middleware called");
     console.log(req.originalUrl);
     console.log("before");
-    next();
+    if(req.query.user === "admin"){
+        next();
+    }else{
+        res.send("<h1>Admin can only access</h1>");
+    }
     console.log("after");
 }
 const defaultResponse = function (requestObject,responseObject,nextMiddleware){
@@ -17,7 +21,8 @@ const defaultResponse = function (requestObject,responseObject,nextMiddleware){
     responseObject.send("<h1>Default home page</h1>")
 }
 // routing level middleware
-app.get("/",middleware,defaultResponse);
+app.get("/",defaultResponse);
+
 function middleware(req,res,next){
     console.log("routing level middleware called");
     next();
@@ -26,9 +31,14 @@ function middleware(req,res,next){
 app.get('/login',auth,(req,res,next)=>{
     res.send("<h1>Login page</h1>")
 })
+
 function auth(req,res,next){
     console.log("auth middleware function called");
-    next();
+    if(req.query.username === "abc" && req.query.password === "abc"){
+        next();
+    } else{
+        res.send("<h1>Login failed</h1>");
+    }
 }
 
 app.listen(port,()=>{
