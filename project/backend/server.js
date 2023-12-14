e3a375dead94593a52f800130f20fa8200ff20cd
SudeepTrainer <info@icenbake.com>
2023-12-14 17:45:55 +0530

    14-12-2023

--- project/backend/server.js
+++ project/backend/server.js
@@ -1,38 +1,84 @@
 const express = require('express');
-const session = require('express-session');
-const mongoDBSession = require('connect-mongodb-session')(session);
+const bcrypt = require('bcryptjs');
+const cookieParser = require('cookie-parser');
+// const session = require('express-session');
+// const mongoDBSession = require('connect-mongodb-session')(session);
 const mongoose = require('mongoose');
 const databaseUri = 'mongodb://127.0.0.1:27017/mydb'
 db().then(res=>console.log("connected")).catch(err => console.log(err));
 async function db() {
   await mongoose.connect(databaseUri);
 }
-const store = new mongoDBSession({
-    uri:databaseUri,
-    collection:"sessions"
-})
+// const store = new mongoDBSession({
+//     uri:databaseUri,
+//     collection:"sessions"
+// })
 const PORT = 3000;
 const app = express();
-//middleware
-app.use(session({
-    secret:'thisisforsigningcookie',
-    resave:false,
-    saveUninitialized:false,
-    cookie:{
-        secure:false,
-        maxAge:60000
-    },
-    store:store
-}));
 
-// routing
-app.get("/",(req,res)=>{
-    req.session.isAuth = true;
-    console.log(req.session);
-    console.log(req.session.id);
-    res.send("Home page");
+// Define the user schema
+const userSchema = new mongoose.Schema({
+    username:String,
+    password:String
 })
 
+// create the user model
+const User = mongoose.model("User",userSchema);
+
+//middleware
+app.use(cookieParser());
+app.use(express.urlencoded({extended:true}));
+app.set('view engine','ejs');
+// app.use(session({
+//     secret:'thisisforsigningcookie',
+//     resave:false,
+//     saveUninitialized:false,
+//     cookie:{
+//         secure:false,
+//         maxAge:60000
+//     },
+//     store:store
+// }));
+
+// routing
+app.get("/login",(req,res)=>{
+    // req.session.isAuth = true;
+    // console.log(req.session);
+    // console.log(req.session.id);
+    res.render('login');
+})
+
+app.get("/register",(req,res)=>{
+    res.render('register')
+})
+
+app.post("/register",async (req,res)=>{
+    const {username,password} = req.body;
+    try{
+        // check if the req body has username and password
+        if(!username || !password){
+            throw new Error('Enter username and password');
+        }
+        // check if username already exists in DB
+        const existingUser = await User.findOne(({username}));
+        if(existingUser){
+            res.status(400).render('register',{'error':'Username already exists'})
+            return
+        }
+        // encrypt the password
+        const hashedPassword = bcrypt.hashSync(password,12);
+        const newUser = new User({
+            username,
+            password:hashedPassword
+        })
+        // save to DB
+        await newUser.save();
+        res.status(201).redirect('/login');
+    }catch(error){
+        console.log(error);
+        res.status(500).render('register',{'error':'Internal server error'})
+    }
+})
 //start server
 app.listen(PORT,()=>{
     console.log(`Listening on ${PORT}`);
