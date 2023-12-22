4f7971a4820e69622c7fa2de52184c92d66e706e
SudeepTrainer <info@icenbake.com>
2023-12-22 16:48:32 +0530

    22-12-2023

--- project/backend/server.js
+++ project/backend/server.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const User = require("./models/user");
 const Product = require("./models/product");
+const CartItem = require('./models/cartItem');
 const databaseUri = 'mongodb://127.0.0.1:27017/mydb'
 db().then(res=>console.log("connected")).catch(err => console.log(err));
 async function db() {
@@ -53,14 +54,12 @@ const isAuthenticated = (req,res,next)=>{
 //     store:store
 // }));
 
-app.get("/",isAuthenticated,async (req,res)=>{
-    try{
-        const products = await Product.find({});
-        res.render('home',{products});
-    }catch(error){
-        console.log(error);
-        res.status(500).render("home",{"error":"Internal server error"})
-    }
+app.get('/', isAuthenticated, async (req, res) => {
+    const products = await Product.find({})
+    // Fetch cart items from the MongoDB collection
+    const cartItems = await CartItem.find({}).populate('product');
+    res.render('home', { products, cart: cartItems });
+
 })
 
 app.get('/logout',(req,res)=>{
@@ -128,6 +127,59 @@ app.post("/register",async (req,res)=>{
         res.status(500).render('register',{'error':'Internal server error'})
     }
 })
+
+// Get Cart
+app.get('/cart', async (req, res) => {
+    try {
+        // Fetch cart items from the MongoDB collection
+        const cartItems = await CartItem.find({}).populate('product');
+
+        // Calculate total cart value
+        const total = cartItems.reduce((acc, item) => acc + item.quantity * item.product.price, 0);
+
+        res.render('cart', { cart: cartItems, total });
+    } catch (error) {
+        console.error('Error fetching cart items:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+// add to cart post
+app.post('/addToCart', async (req, res) => {
+    try {
+        const productId = req.body.productId;
+        const quantity = parseInt(req.body.quantity);
+
+        // Fetch the selected product
+        const product = await Product.findById(productId);
+
+        if (!product) {
+            return res.status(404).send('Product not found.');
+        }
+
+        // Check if the item is already in the cart
+        const existingCartItem = await CartItem.findOne({ product: productId });
+
+        if (existingCartItem) {
+            // Update the quantity if the item is already in the cart
+            existingCartItem.quantity += 1;
+            await existingCartItem.save();
+        } else {
+            // Add a new item to the cart
+            await CartItem.create({
+                product: productId,
+                quantity: quantity
+            });
+        }
+
+        res.redirect('/');
+    } catch (error) {
+        console.error('Error adding to cart:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 //start server
 app.listen(PORT,()=>{
     console.log(`Listening on ${PORT}`);
